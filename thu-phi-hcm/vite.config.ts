517fb163b36ddd99a2d6ad13991d1df6f7127f88
DESKTOP-BEGCN1S\Thai Vu <thaivh19@gmail.com>
2025-09-10 10:04:37 +0700

    no message

--- thu-phi-hcm/vite.config.ts
+++ thu-phi-hcm/vite.config.ts
@@ -12,5 +12,12 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/PHT_BE/api')
+      }
+    }
   },
 });
